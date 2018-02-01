fdea99931fee4f33909e68d3d9e9999f3ce9de96
Jessica Pergentino <jessicapergentino@hotmail.com>
2018-02-01 18:29:52 -0300

    Componente TabHeader conectado com o redux - action creator selectedTab feita

--- frontend/src/common/tab/tabActions.js
+++ frontend/src/common/tab/tabActions.js
@@ -1,7 +1,6 @@
 // funções que irão gerar as ações e que irão atualizar o estado - Dispara a ação
 
 export function selectTab(tabId){
-    console.log(tabId)
     return { // retorna um objeto
         type:'TAB_SELECTED', // Ação que será disparada
         payload: tabId //id da aba selecionada - será retornado para o reducer que gerencia o estado da aba
--- frontend/src/common/tab/tabHeader.jsx
+++ frontend/src/common/tab/tabHeader.jsx
@@ -2,12 +2,19 @@
 
 import React, {Component} from 'react'
 
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+
+import {selectTab} from './tabActions' // import da ação
+
 class TabHeader extends Component {
     render() {
+        const selected = this.props.tab.selected === this.props.target // selected é um boolean que verifica se dentro do estado tab o atributo selected é igual ao target - se for igual quer dizer que essa aba foi selecionada
         return (
-            <li>
+            <li className={selected ? 'active' : ''} >{/*ClassName condicional para saber se a constante selected é verdadeira, se for a aba terá a aparencia de ativada, se não o className será vazio*/}
                 <a href='javascript:;' 
-                    data-toggle='tab' 
+                    data-toggle='tab'
+                    onClick={() => this.props.selectTab(this.props.target)} //A partir do click é disparada a action passando como parametro o target(id)
                     data-target={this.props.target}>{/*Irá garantir que o conteudo exbido será dessa aba, atravez do id que será passado pelo target*/}
                     <i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
                     </a>
@@ -16,4 +23,7 @@ class TabHeader extends Component {
     }
 }
 
-export default TabHeader
+const mapStateToProps = state => ({tab: state.tab}) // Mapeamento para o componente ter acesso ao estado tab - state.tab vem do reducers que é gerenciado pelo tabReducer
+const mapDispatchToProps= dispatch => bindActionCreators({selectTab}, dispatch) //Mapeamento do action creator para que ele possa ser disparado e disponivel a partir das propiedades desse componente - o dispatch chama um action creator que tem como resultado uma action e a action é passada para os reducers e então estado é evoluido e os componentes são re-renderizados
+
+export default connect(mapStateToProps,mapDispatchToProps)(TabHeader) // decora o compenente para que ele tenha acesso as novas propriedades e conecta o componente com o estado e as actions
