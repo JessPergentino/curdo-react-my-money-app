8c8eaa6ff12206688c0f3da6ccf5b03d2f60122e
Jessica Pergentino <jessicapergentino@hotmail.com>
2018-02-01 19:02:26 -0300

    componente billingCycle conectado com o redux para a utilização da action creator

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -2,17 +2,27 @@
 
 import React, {Component} from 'react'
 
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+
 import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 
+import {selectTab} from '../common/tab/tabActions'
+
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 
 class BillingCycle extends Component {
-   render() {
+   
+    componentWillMount() { //Metodo que sempre é chamado quando o componente é renderizado
+        this.props.selectTab('tabList') // A partir das propiedades dispara o action creator - Faz com que a aba de listar seja selecionada por padrão sempre que o componente for renderizado na primeira vez ou com um refresh
+    }
+   
+    render() {
        return (
            <div>
             <ContentHeader title='Ciclos de Pagamentos' small='Cadastro'/>
@@ -37,4 +47,5 @@ class BillingCycle extends Component {
    }
 }
 
-export default BillingCycle
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch) // Liga ao action creator deixando o selectTab disponivel nas propiedades do componente
+export default connect(null, mapDispatchToProps)(BillingCycle) // Expota o componente e faz a conexão
