4f211d7e0c9dec7157fe6d73d3f792fc08976113
Jessica Pergentino <jessicapergentino@hotmail.com>
2018-02-19 17:27:26 -0300

    campo status adicionado ao itemList

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -12,7 +12,7 @@ import {init} from './billingCycleActions'
 class BillingCycleForm extends Component{
     render() {
 
-        const {handleSubmit, readOnly, credits} = this.props // metodo do reduxform para processamento do formulario
+        const {handleSubmit, readOnly, credits, debts} = this.props // metodo do reduxform para processamento do formulario
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>{/*Recebe como parametro a action que será disparada*/}
@@ -24,6 +24,8 @@ class BillingCycleForm extends Component{
                         label='Ano' cols='12 4' placeholder='Informe o ano' />
                     <ItemList cols='12 6' list={credits} readOnly={readOnly}
                         legend='Créditos' field='credits' />
+                     <ItemList cols='12 6' list={debts} readOnly={readOnly}
+                        legend='Débitos' field='debts' showStatus={true} />
                 </div>
 
                 <div className='box-footer' >
@@ -38,6 +40,9 @@ class BillingCycleForm extends Component{
 
 BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm) //Ligação com o estado do redux - o segundo parametro é para manter os dados do formulario mesmo que o componente tenha sido destruido na passagem de abas - passa para uma variavel o resultado 
 const selector = formValueSelector('billingCycleForm') // função do reduxform passando o id do formulario que eu quero pegar a infomação - selector é usado dentro do metodo mapStateToProps
-const mapStateToProps = state => ({credits: selector(state, 'credits')}) // a função selector tem o estado como parametro e o nome do atributo que queremos extrair do formulario - o resultado é colocado nas propiedades do componente
+const mapStateToProps = state => ({
+    credits: selector(state, 'credits'),
+    debts: selector(state, 'debts')
+}) // a função selector tem o estado como parametro e o nome do atributo que queremos extrair do formulario - o resultado é colocado nas propiedades do componente
 const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
 export default connect(mapStateToProps,mapDispatchToProps) (BillingCycleForm)
