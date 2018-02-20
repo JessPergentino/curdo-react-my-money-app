64f074c2deffc7216f867adfd2b53e4805159d44
Jessica Pergentino <jessicapergentino@hotmail.com>
2018-02-20 18:01:27 -0300

    conclusão  do authService - criação do sigup

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -45,3 +45,53 @@ const validateToken = (req, res, next) => {
         return res.status(200).send({valid: !err}) // retorna a resposta se não tiver erro retorna true se tiver retorna false
     })
 }
+
+// metodo de sigup
+const sigup = (req, res, next) => {
+// conjunto de dados recebido do formulario de criação do usuario
+    const name = req.body.name || ''
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+    const confirmPassword = req.body.confirm_password || ''
+
+    //metodo de validação de email
+    if(!email.match(emailRegex)) { // se o email não bater com o emailRegex será retornado um erro
+        return res.status(400).send({errors: ['O email informado está inválido']})
+    }
+
+    //metodo de validação da senha
+    if(!password.match(passwordRegex)) { // se a senha não bater com a passwordRegex será retonado um erro
+        return res.status(400).send({
+            errors: [
+                "Senha precisa ter: uma letra maiúscula, uma letra minúscula, um número, um caractere especial(@#$%) e tamanho entre 6-20"
+            ]
+        })
+    }
+
+    //Geração do hash da senha
+    const salt = bcrypt.genSaltSync()
+    const passwordHash = bcrypt.hashSync(password, salt) // geração do hash
+    if (!bcrypt.compareSync(confirmPassword, passwordHash)) { // utilizando o bcrypt para gerar o hash confirmPassword e comparar com o passwordHash
+        return res.status(400).send({errors: ['Senhas não conferem.']}) // se der errado retorna o erro
+    }
+
+    //verifica se o usuario já esta cadastrado
+    User.findOne({email}, (err,user) => { // pesquisa pelo email
+        if(err) { // se houver um erro retorna o erro
+            return sendErrorsFromDB(res, err)
+        } else if (user) { // se encontrsr o usuário
+            return resizeBy.status(400).send({errors: ['Usuário já cadastrado.']}) // retorna a resposta de que já existe esse usuario
+        } else { // caso não ache nenhum usuario, ai sim cadastra o usuario 
+            const newUser = new User({name, email, password: passwordHash}) // cria uma constate para o novo usuario, com nome, email e o hash da senha
+            newUser.save(err => {
+                if (err) { // verifica se tem um erro
+                    return sendErrorsFromDB(res,err) // se tiver manda o erro
+                } else { // se não houver erro
+                    login(req,res,next) // chama o metodo login
+                }
+            })
+        }
+    })
+}
+
+module.exports = { login, sigup, validateToken} // exporta somente esses metodos
