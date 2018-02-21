3ef31b6dad06dead8752ada3d4e54537ef41fb16
Jessica Pergentino <jessicapergentino@hotmail.com>
2018-02-21 10:14:17 -0300

    correções de sintax

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -7,7 +7,7 @@ const User = require('./user')
 const env = require('../../.env')
 
 const emailRegex = /\S+@\S+\.\S+/ // Expressão regular usada para validar o email - verifica se tem uma sequencia de caracteres antes do arroba se tem dps e se tem dps do ponto mais uma sequencia de caracteres
-const passwordRegex = /((?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/ // Expressão regular para validar a senha - verifica se possui digitos, se possui letras minusculas, se possui letras maiusculas, se possui caracteres especiais e se possui de 6 a 20 caracteres
+const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/ // Expressão regular para validar a senha - verifica se possui digitos, se possui letras minusculas, se possui letras maiusculas, se possui caracteres especiais e se possui de 6 a 20 caracteres
 
 // metodo para tratamento de erros do banco de dados
 const sendErrorsFromDB = (res, dbErrors) => {
