9ff21eae42390aa4fed40e3eb9389949cfc8ced8
Jessica Pergentino <jessicapergentino@hotmail.com>
2018-02-22 20:19:49 -0300

    Correções - aplicação ainda não renderizando

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,7 +1,7 @@
 // Configuração de rotas
 
 import React from 'react'
-import { Router, Route, Redirect, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
 import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
@@ -10,8 +10,8 @@ import AuthOrApp from './authOrApp'
 
 export default props => (
     <Router history={hashHistory}>{/* Criação das rotas */}
-        <Route path='/' component={authOrApp}>
-            <Route path='/' component={Dashboard} />{/* Para o caminho / va para o componente dashboard */}
+        <Route path='/' component={AuthOrApp}>
+            <IndexRoute component={Dashboard} />{/* Para o caminho / va para o componente dashboard */}
             <Route path='/billingCycles' component={BillingCycle} />{/* Para o caminho /billingClycls va para o componente billingCycle */}
         </Route>
         <Redirect from='*' to='/' />{/* Para qualquer outro caminho (*) redirecione para o dashboard */}
