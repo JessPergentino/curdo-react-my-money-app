d80a8e47488960c228432c34578c07d1e67678bb
Jessica Pergentino <jessicapergentino@hotmail.com>
2018-02-22 18:44:08 -0300

    Craição do esboço do navbar

--- frontend/src/common/template/header.jsx
+++ frontend/src/common/template/header.jsx
@@ -1,6 +1,8 @@
 //component funcional do react - Cabeçalho da Aplicação
 import React from 'react' //referencia para o react
 
+import Navbar from './navbar'
+
 export default props => (
     <header className='main-header' >
         <a href='/#/' className='logo'>
@@ -13,6 +15,7 @@ export default props => (
 
         <nav className='navbar navbar-static-top' >
             <a href='' className='sidebar-toggle' data-toggle='offcanvas'></a>
+            <Navbar/>
         </nav>
     </header>
 )
