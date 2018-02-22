e1062aa6da6c00492cf86da295ecc2608dd1acf8
Jessica Pergentino <jessicapergentino@hotmail.com>
2018-02-22 19:01:21 -0300

    Criação do componente de input para autorização

---
+++ frontend/src/common/form/inputAuth.jsx
@@ -0,0 +1,16 @@
+// componente de input de autenticação - para login e signup
+
+import React from 'react'
+import If from '../operador/if'
+
+export default props => (
+    <If test={!props.hide}>
+        <div className="form-group has-feedback" >
+            <input {...props.input}
+            className={props.placeholder}
+            readOnly={props.readOnly}
+            type={props.type}/>
+        <span className={`glyphicon glyphicon-${props.icon} form-control-feedback`} ></span>
+        </div>
+    </If>
+)   
